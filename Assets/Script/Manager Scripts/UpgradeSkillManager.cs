b016d8d987b659ea00f3d097fdedc16ff19f4a00
Zayd <zayd.codes@gmail.com>
2026-05-26 17:08:24 +0400

    Implement Upgrade Skill List Scriptable Object

--- Assets/Script/Manager Scripts/UpgradeSkillManager.cs
+++ Assets/Script/Manager Scripts/UpgradeSkillManager.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 public class UpgradeSkillManager : MonoBehaviour
 {
     public static UpgradeSkillManager Instance {get; private set;}
-    [SerializeField] private List<UpgradeSkillSO> upgradeSkillSOList;
+    [SerializeField] private UpgradeSkillListSO upgradeSkillListSO;
+    private List<UpgradeSkillSO> upgradeSkillSOList;
+    private int upgradeSkillMax = 3;
+    private int index;
     private void Awake()
     {
         Instance = this;
+        upgradeSkillSOList = new List<UpgradeSkillSO>();
+        SetUpgradeSkillList(index);
+    }
+    
+    private void SetUpgradeSkillList(int index)
+    {
+        while (index < upgradeSkillMax)
+        {
+            UpgradeSkillSO upgradeSkillSO = upgradeSkillListSO.upgradeSkillListSO[Random.Range(0, upgradeSkillListSO.upgradeSkillListSO.Count)];
+            upgradeSkillSOList.Add(upgradeSkillSO);
+            index++;
+        }
+        
     }
     public List<UpgradeSkillSO> GetUpgradeSkillList()
     {
